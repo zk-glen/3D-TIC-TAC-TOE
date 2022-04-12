e0fc898b973817af5c480946883e4c9d9bc848c2
Glen Maytum <glenmaytum@gmail.com>
2022-04-12 19:24:33 +0100

    done normal game, basic styles, and functionality

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,5 +1,24 @@
-function Square() {
-  return <button>This is a square</button>;
+type Player = "X" | "O" | "DRAW" | null;
+
+function Square({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button className="square" onClick={onClick} disabled={Boolean(winner)} />
+    );
+  }
+  return (
+    <button className={`square square_${value.toLowerCase()}`} disabled>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
